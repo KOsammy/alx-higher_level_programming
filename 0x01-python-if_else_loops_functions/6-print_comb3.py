d28dbd5b2f5581da23115e29fb554eadd09175b9
KOsammy <samoppong770@gmail.com>
2024-09-10 20:19:29 -0700

    Removed the trailing comma in the code

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        if i < 8 or (i == 8 and j == 9):
-            print("{:02d}, ".format(i * 10 + j), end="")
+        if i == 8 and j == 9:
+            print("{:02d}{:02d}".format(i, j))
         else:
-            print("{:02d}".format(i * 10 + j))
+            print("{:02d}{:02d}, ".format(i, j), end="")
